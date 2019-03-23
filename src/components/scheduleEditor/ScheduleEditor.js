6c88e754f44f765ce9ef034008c4e3f79095c1ca
Claudio Garaycochea <computadoraweb@gmail.com>
2019-03-23 14:37:22 -0300

    Schedule font

--- src/components/scheduleEditor/ScheduleEditor.js
+++ src/components/scheduleEditor/ScheduleEditor.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import './ScheduleEditor.css';
 
 class ScheduleEditor extends Component {
   render() {
     return (
-      <div>
-        ScheduleEditor
+      <div className="schedule-editor">
+        <div className="module-box">
+          <div className="header">
+            ScheduleEditor HEader
+          </div>
+          <div className="content">
+            ScheduleEditor content
+          </div>
+        </div>
       </div>
     );
   }
