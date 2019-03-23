c25c97e1354faa04806caf076341977495664541
Claudio Garaycochea <computadoraweb@gmail.com>
2019-03-23 16:49:45 -0300

    Group visual

--- src/components/groupSelector/GroupSelector.js
+++ src/components/groupSelector/GroupSelector.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import './GroupSelector.css';
 
+const groupList = [
+  {
+    id: 1,
+    name: 'Investors',
+    color: 'green',
+  },
+  {
+    id: 2,
+    name: 'Developers',
+    color: 'violet',
+  },
+  {
+    id: 3,
+    name: 'Investors & Devs',
+    color: 'blue',
+  }
+];
+
 class GroupSelector extends Component {
   render() {
     return (
@@ -10,8 +28,12 @@ class GroupSelector extends Component {
             GROUPS
           </div>
           <div className="content">
-            Group content
+            { groupList.map((item, key)=>{
+                return <div key={key} className={`group-item ${item.color}`}>{item.name}</div>
+              })
+            }
           </div>
+          <div className="footer"><button className="btn large-full">Add Group</button></div>
         </div>
       </div>
     );
