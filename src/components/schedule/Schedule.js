16a7d2de54a1fc6b3b87eef46920aa1c0dae5bc1
Claudio Garaycochea <computadoraweb@gmail.com>
2019-03-23 12:04:09 -0300

    Schedule base

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import './Schedule.css';
 
 class Schedule extends Component {
   render() {
     return (
-      <div className="content-page">
-        Schedule
+      <div className="content-page schedule">
+        <div className="schedule-side">
+          {/*<SideMenu />*/}
+          Schedule side
+        </div>
+        <div className="schedule-content">
+          Schedule Editor
+          {/*<HeaderPage />
+          <Schedule />*/}
+        </div>
       </div>
     );
   }
